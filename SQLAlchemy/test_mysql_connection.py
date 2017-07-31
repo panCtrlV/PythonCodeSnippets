7e959da5e6cf36ce3e85b8fff10c804d298d275d
Pan Chao <panchao25@gmail.com>
2017-07-30 21:02:12 -0400

    Added prerequisite.

--- SQLAlchemy/test_mysql_connection.py
+++ SQLAlchemy/test_mysql_connection.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
 '''
+Pre-requisite:
+
+On Python 3.X, make sure to install mysqlclient package:
+
+    pip install mysqlclient
+
 Quickly test if the connection to a MySQL database is successful. 
 Useful when you want to access the MySQL db in your application.
 
@@ -9,7 +15,7 @@ If the following code prints results, then the connection is successful.
 @Author: panc
 @Date:   2017-07-30 19:20:27
 @Last Modified by:   Pan Chao
-@Last Modified time: 2017-07-30 20:59:15
+@Last Modified time: 2017-07-30 21:01:17
 '''
 
 from sqlalchemy import create_engine
