0ecf73775e836e75114e752a532b2712ef8066ca
Pan Chao <panchao25@gmail.com>
2017-07-31 00:33:42 -0400

    Bug fix.

--- SQLAlchemy/test_mysql_connection.py
+++ SQLAlchemy/test_mysql_connection.py
@@ -15,14 +15,14 @@ If the following code prints results, then the connection is successful.
 @Author: panc
 @Date:   2017-07-30 19:20:27
 @Last Modified by:   Pan Chao
-@Last Modified time: 2017-07-30 21:01:17
+@Last Modified time: 2017-07-31 00:33:17
 '''
 
 from sqlalchemy import create_engine
 
 
 engine = create_engine('mysql://panc:<my_passowrd>@localhost/rongzhongdb')
-connection = engine.connection()
+connection = engine.connect()
 result = connection.execute('select * from data_source')
 for row in result:
     print('field_name:', row['field_name'])
