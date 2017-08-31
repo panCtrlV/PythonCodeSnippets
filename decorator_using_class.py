9ff5e7f9db00d379e63a49645e67decfbff80c70
Pan Chao <panchao25@gmail.com>
2017-08-31 12:00:14 -0400

    Minor change

--- decorator_using_class.py
+++ decorator_using_class.py
@@ -10,9 +10,9 @@ class entry_exit(object):
     def __init__(self, f):
         self.f = f
 
-    def __call__(self):
+    def __call__(self, *args):
         print('Entering', self.f.__name__)
-        self.f()
+        self.f(*args)
         print('Exited', self.f.__name__)
 
 
